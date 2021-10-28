f2e1c23ad7aca0e8f67aa589b6e681fbea31ce34
shreyansh018 <shreyansh.singhrajput2019@gmail.com>
2021-10-28 23:29:35 +0530

    complete build

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAn0uvnVzWDI_yYgYkbjrQcU74BZTAYk9s",
-  authDomain: "instagram-clone-313de.firebaseapp.com",
-  projectId: "instagram-clone-313de",
-  storageBucket: "instagram-clone-313de.appspot.com",
-  messagingSenderId: "305533584034",
-  appId: "1:305533584034:web:e2efb64096e344f508cdfb",
+  apiKey: "AIzaSyDwSubBcZ6h7LL8rW5IMwDf-lqarK-LisQ",
+  authDomain: "insta-clone-c5e1c.firebaseapp.com",
+  projectId: "insta-clone-c5e1c",
+  storageBucket: "insta-clone-c5e1c.appspot.com",
+  messagingSenderId: "58795945555",
+  appId: "1:58795945555:web:d76fd18a2ae90fef3ea0a6",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
